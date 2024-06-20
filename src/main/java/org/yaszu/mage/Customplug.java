fd767a9b9b6840839eb2ff40dca8cfbe660c11cd
Yaszu <76853160+Dawn-Shalve@users.noreply.github.com>
2024-06-20 14:07:13 -0700

    Initial

--- src/main/java/org/yaszu/mage/Customplug.java
+++ src/main/java/org/yaszu/mage/Customplug.java
@@ -6,7 +6,7 @@ public final class Customplug extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        System.out.println("Plugin is working!!");
 
     }
 

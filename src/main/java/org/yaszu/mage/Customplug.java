84244bad7e93963521bd13efd2cfc9f99a055b52
Yaszu <76853160+Dawn-Shalve@users.noreply.github.com>
2024-06-20 14:03:05 -0700

    Initial

---
+++ src/main/java/org/yaszu/mage/Customplug.java
@@ -0,0 +1,17 @@
+package org.yaszu.mage;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Customplug extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
